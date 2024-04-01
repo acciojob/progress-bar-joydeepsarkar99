f9c8ae528afb6dbf8cf713c97404ad871ddfab21
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-01 16:09:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your JS code here. If required.
+let prevbtn = document.querySelector("#prev")
+let nextbtn = document.querySelector("#next")
+let circles = document.querySelectorAll(".circle")
+let lines = document.querySelectorAll(".line")
+
+prevbtn.disabled = true
+
+let totalCount = 0
+for(let i of circles){
+    totalCount++
+}
+let count = 0
+nextbtn.addEventListener("click",()=>{
+    if(count < totalCount-1){
+        count++
+        if(count == totalCount-1){
+            nextbtn.disabled = true
+        }
+    }
+    if(count > 0){
+        prevbtn.disabled = false
+    }
+    let currCircle = circles[count]
+    let currLine = lines[count-1]
+    if(count < totalCount){
+        currCircle.classList.add("active")
+        currLine.style.cssText = `background-color: #3498DB;`
+    }
+})
+prevbtn.addEventListener("click",()=>{
+    let currCircle = circles[count]
+    let currLine = lines[count-1]
+    currCircle.classList.remove("active")
+    currLine.style.cssText = `background-color: #ccc;`
+    count--
+    if(count == totalCount-2){
+        nextbtn.disabled = false
+    }
+    if(count == 0){
+        prevbtn.disabled = true
+    }
+})
+
+
